d7b41d0401539d00204117cfb0039e7b2b7ad3d7
Eric Blau <blau@anl.gov>
2023-02-23 12:20:33 -0600

    Get list of user's groups using dependent token

--- search_ingest_lambda_function.py
+++ search_ingest_lambda_function.py
@@ -5,6 +5,7 @@ import json
 
 SEARCH_INDEX_UUID = '58e4df29-4492-4e7d-9317-b27eba62a911'
 
+
 def get_secret():
     secret_name = "arn:aws:secretsmanager:us-east-2:509474786919:secret:GlobusAuthHelloWorldSecret-0q5j66"
     region_name = "us-east-2"
@@ -23,35 +24,52 @@ def get_secret():
     return eval(get_secret_value_response['SecretString'])
 
 
+def _get_group_ids_groups_api(token):
+    # Create a Groups client to retrieve the user's groups
+    groups_client = globus_sdk.GroupsClient(
+        authorizer=globus_sdk.AccessTokenAuthorizer(token))
+    user_groups = groups_client.get_my_groups()
+    user_group_ids = {_["id"] for _ in user_groups}
+    return user_group_ids
+
 
 def lambda_handler(event, context):
-    print("Event received: {} \n".format(event))
-    print("event body: {} \n".format(event['body']))
+    # print("Event received: {} \n".format(event))
+    # print("event body: {} \n".format(event['body']))
+
     # Get the user's access token from the event data
     # access_token = event['access_token']
-   
-    # Get the CLIENT_ID and SECRET 
+
+    # Get the CLIENT_ID and SECRET
     globus_secrets = get_secret()
 
     # Validate the user's access token with Globus Auth
     auth_client = globus_sdk.ConfidentialAppAuthClient(
         globus_secrets['API_CLIENT_ID'], globus_secrets['API_CLIENT_SECRET'])
 
-    print("established auth_client\n")
-
     token = event['headers']['authorization'].replace("Bearer ", "")
 
-    auth_res = auth_client.oauth2_token_introspect(token, include="identities_set")
-    print("auth_res: {} \n".format(auth_res))
+    auth_res = auth_client.oauth2_token_introspect(
+        token, include="identities_set")
+    # print("auth_res: {} \n".format(auth_res))
     if not auth_res['active']:
         raise Exception("Invalid access token")
+
+    # Get dependent tokens
+    dep_tokens = auth_client.oauth2_get_dependent_tokens(token)
+
+    if "groups.api.globus.org" in dep_tokens.by_resource_server:
+        groups_token = dep_tokens.by_resource_server["groups.api.globus.org"]["access_token"]
+        user_group_ids = _get_group_ids_groups_api(groups_token)
+        # print(f"User's group ids: {user_group_ids}")
+
+    # Perform whatever policy checks we are going to do w/r/t write access
+    # To determine whether this should be written
+    # For example, check if the user is in the right group
 
     # Get the document from the event data
     body = json.loads(event['body'])
     document = body['document_file']
-
-    # Perform whatever policy checks we are going to do w/r/t write access
-    # To determine whether this should be written
 
     # Create a Globus Search API client using the client credentials of the lambda function
     scopes = (globus_sdk.scopes.SearchScopes.all)
@@ -60,7 +78,6 @@ def lambda_handler(event, context):
     search_client = globus_sdk.SearchClient(authorizer=cc_authorizer)
 
     # Ingest the document file into Globus Search
-    res = search_client.ingest(SEARCH_INDEX_UUID,document)
+    res = search_client.ingest(SEARCH_INDEX_UUID, document)
 
     return res.data
-
